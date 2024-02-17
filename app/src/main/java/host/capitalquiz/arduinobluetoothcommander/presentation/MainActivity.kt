90062a8e12f26d547bba9e59723ebcb361913b94
Alex <al3xxxgm@gmail.com>
2024-02-17 17:02:33 +0300

    move MainActivity to presentation

--- app/src/main/java/host/capitalquiz/arduinobluetoothcommander/presentation/MainActivity.kt
+++ app/src/main/java/host/capitalquiz/arduinobluetoothcommander/presentation/MainActivity.kt
@@ -1,8 +1,9 @@
-package host.capitalquiz.arduinobluetoothcommander
+package host.capitalquiz.arduinobluetoothcommander.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,20 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import host.capitalquiz.arduinobluetoothcommander.ui.theme.ArduinoBluetoothCommanderTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ArduinoBluetoothCommanderTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                val viewModel = viewModels<BluetoothViewModel>()
             }
         }
     }
