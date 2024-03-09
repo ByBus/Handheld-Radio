a3cba690bda6abfe1f49d85c5800cfe31350fe27
Alex <al3xxxgm@gmail.com>
2024-03-09 19:36:52 +0300

    add fonts

---
+++ app/src/main/java/host/capitalquiz/arduinobluetoothcommander/ui/theme/Typography.kt
@@ -0,0 +1,14 @@
+package host.capitalquiz.arduinobluetoothcommander.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import host.capitalquiz.arduinobluetoothcommander.R
+
+val adventProFamily = FontFamily(
+    Font(R.font.adventpro_semiexpanded_light, FontWeight.Light),
+    Font(R.font.adventpro_semiexpanded_regular, FontWeight.Normal),
+    Font(R.font.adventpro_semiexpanded_medium, FontWeight.Medium),
+    Font(R.font.adventpro_semiexpanded_semibold, FontWeight.Bold),
+    Font(R.font.adventpro_semisxpanded_extrabold, FontWeight.ExtraBold)
+)
